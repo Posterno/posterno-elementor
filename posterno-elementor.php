063337ebd331227efe05364318e9871fac23783c
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-01-14 16:26:43 +0000

    hello world

---
+++ posterno-elementor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:     Posterno Elementor
+ * Plugin URI:      https://posterno.com/extensions/elementor
+ * Description:     Visually build your listings directory website with Posterno and Elementor.
+ * Author:          Posterno
+ * Author URI:      https://posterno.com
+ * Text Domain:     posterno-elementor
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * Posterno Elementor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Posterno Elementor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PosternoElementor. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package posterno-elementor
+ * @author Sematico LTD
+ */
+
+defined( 'ABSPATH' ) || exit;
