4a06c049d4b9e97b9e8d8acd3589e173436e48e7
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-01-20 16:43:39 +0000

    added layout for listings types

--- templates/terms-list-listings-locations.php
+++ templates/terms-list-listings-locations.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying the content of the listings categories list shortcode.
  *
- * This template can be overridden by copying it to yourtheme/posterno/shortcodes/listings-categories.php
+ * This template can be overridden by copying it to yourtheme/posterno/shortcodes/listings-locations.php
  *
  * HOWEVER, on occasion PNO will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
