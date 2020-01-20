28449b186835df4d7c8078e17d11260463628953
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-01-20 16:52:58 +0000

    added template for default terms list

--- templates/terms-list-default.php
+++ templates/terms-list-default.php
@@ -14,7 +14,34 @@
  * @package posterno-elementor
  */
 
-use Posterno\Elementor\Helper;
-
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+$terms = get_terms(
+	array(
+		'taxonomy'   => isset( $data->taxonomy_id ) && ! empty( $data->taxonomy_id ) ? esc_attr( $data->taxonomy_id ) : false,
+		'hide_empty' => isset( $data->hide_empty ) && $data->hide_empty === 'yes' ? true : false,
+		'parent'     => 0,
+		'number'     => isset( $data->number ) && ! empty( $data->number ) ? absint( $data->number ) : 999,
+		'orderby'    => isset( $data->orderby ) && ! empty( $data->orderby ) ? esc_attr( $data->orderby ) : 'name',
+		'order'      => isset( $data->order ) && ! empty( $data->order ) ? esc_attr( $data->order ) : 'ASC',
+	)
+);
+
+$i = 0;
+
+if ( ! is_array( $terms ) || empty( $terms ) ) {
+	return;
+}
+
+?>
+
+<div class="pno-listings-terms-list">
+
+	<?php foreach ( $terms as $term ) : ?>
+
+		<a href="<?php echo esc_url( get_term_link( $listing_type ) ); ?>" class="badge badge-secondary pt-2 pr-3 pl-3 pb-2 mb-1 ml-1 text-white font-weight-normal"><?php echo esc_html( $term->name ); ?></a>
+
+	<?php endforeach; ?>
+
+</div>
