44e86cfc549e5bf6d241b3c250ab971578306447
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-01-24 11:57:36 +0000

    prototype logic detection for visibility

--- includes/classes/Visibility.php
+++ includes/classes/Visibility.php
@@ -49,6 +49,12 @@ class Visibility {
 		add_action( 'elementor/element/section/section_advanced/after_section_end', array( $this, 'register_section' ) );
 		add_action( 'elementor/element/common/posterno_visibility_section/before_section_end', array( $this, 'register_controls' ), 10, 2 );
 		add_action( 'elementor/element/section/posterno_visibility_section/before_section_end', array( $this, 'register_controls' ), 10, 2 );
+
+		add_filter('elementor/widget/render_content', [ $this, 'content_change' ], 999, 2 );
+		add_filter('elementor/section/render_content', [ $this, 'content_change' ], 999, 2 );
+
+		add_filter( 'elementor/frontend/section/should_render', [ $this, 'section_should_render' ] , 10, 2 );
+
 	}
 
 	/**
@@ -76,7 +82,7 @@ class Visibility {
 		$element->add_control(
 			'posterno_visibility_enabled',
 			array(
-				'label'        => esc_html__( 'Enable Conditions', 'posterno-elementor' ),
+				'label'        => esc_html__( 'Enable visibility conditions', 'posterno-elementor' ),
 				'type'         => Controls_Manager::SWITCHER,
 				'default'      => '',
 				'label_on'     => esc_html__( 'Yes', 'posterno-elementor' ),
@@ -90,7 +96,7 @@ class Visibility {
 			array(
 				'type'      => Controls_Manager::SELECT2,
 				'label'     => esc_html__( 'Visible for:', 'posterno-elementor' ),
-				'options'   => array(),
+				'options'   => $this->get_visibility_options(),
 				'default'   => array(),
 				'multiple'  => true,
 				'condition' => array(
@@ -105,7 +111,7 @@ class Visibility {
 			array(
 				'type'      => Controls_Manager::SELECT2,
 				'label'     => esc_html__( 'Hidden for:', 'posterno-elementor' ),
-				'options'   => array(),
+				'options'   => $this->get_visibility_options(),
 				'default'   => array(),
 				'multiple'  => true,
 				'condition' => array(
@@ -116,5 +122,76 @@ class Visibility {
 		);
 
 	}
+
+	/**
+	 * Get the list of visibility options.
+	 *
+	 * @return array
+	 */
+	private function get_visibility_options() {
+
+		$options = [
+			'user' => esc_html__( 'Logged in' ),
+			'guest' => esc_html__( 'Logged out' ),
+		];
+
+		return apply_filters( 'pno_elementor_visibility_options', $options );
+
+	}
+
+	/**
+	 * Hide content based on selected conditions.
+	 *
+	 * @param string $content the content to output.
+	 * @param object $widget elementor widget instance.
+	 * @return string
+	 */
+	public function content_change( $content, $widget ) {
+
+		if ( \Elementor\Plugin::$instance->editor->is_edit_mode() ) {
+			return $content;
+		}
+
+		$settings = $widget->get_settings();
+
+		if ( ! $this->should_render( $settings ) ) {
+			return '';
+		}
+
+		return $content;
+
+	}
+
+	/**
+	 * Detect whether or not a section should render.
+	 *
+	 * @param bool $should_render whether or not the section should render.
+	 * @param object $section elementor widget instance.
+	 * @return bool
+	 */
+	public function section_should_render( $should_render, $section ) {
+
+		$settings = $section->get_settings();
+
+		if ( ! $this->should_render( $settings ) ) {
+			return false;
+		}
+
+		return $should_render;
+
+	}
+
+	/**
+	 * Process the visibility conditions specified for widgets and sections.
+	 *
+	 * @param array $settings settings list.
+	 * @return boolean
+	 */
+	private function should_render( $settings ) {
+
+		return true;
+
+	}
+
 
 }
