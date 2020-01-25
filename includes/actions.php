11e19af86ef561600a69ddf56b417c56fdf7244f
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-01-25 12:12:07 +0000

    added archive sorter element

--- includes/actions.php
+++ includes/actions.php
@@ -74,6 +74,7 @@ add_action(
 			}
 
 			\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Posterno\Elementor\Elements\Archive\ArchiveMap() );
+			\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Posterno\Elementor\Elements\Archive\ArchiveSorter() );
 
 		}
 
