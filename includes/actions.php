16662f9040aa4fe6bc23b9421e27eaaf4839a1eb
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-01-28 10:30:34 +0000

    added reviews bars element

--- includes/actions.php
+++ includes/actions.php
@@ -90,6 +90,7 @@ add_action(
 				\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Posterno\Elementor\Elements\Single\ListingReviewsFilter() );
 				\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Posterno\Elementor\Elements\Single\ListingReviewsList() );
 				\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Posterno\Elementor\Elements\Single\ListingReviewsForm() );
+				\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Posterno\Elementor\Elements\Single\ListingReviewsBars() );
 			}
 
 		}
